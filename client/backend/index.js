deb757f31f28b491c6cf8cf2b81d3a0f1ea79366
extrude575757 <nicholas.a.lacapria@ssh.hotmail.com>
2022-05-18 19:41:41 -0700

    Only the front ready for heroku deployment

--- client/backend/index.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-
-const server = require("./api/server.js");
-
-const port = process.env.NOPORT || 5075;
-server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
