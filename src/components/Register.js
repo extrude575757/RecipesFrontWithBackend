8c878e4ca3a29dbff1116e9f599e0eac632b7824
extrude575757 <nicholas.a.lacapria@ssh.hotmail.com>
2022-05-19 01:26:13 -0700

    Added in the hostsing names and now I should be able to have a production launch of the database taht is working from the other hosted site at https://backrecipes.herokuapp.com

--- src/components/Register.js
+++ src/components/Register.js
@@ -32,7 +32,7 @@ class Register extends React.Component {
   login = e => {
     e.preventDefault();
     // login to server
-    axiosWithAuth().post("http://localhost:7666/api/auth/register", this.state.credentials)
+    axiosWithAuth().post("https://backrecipes.herokuapp.com/api/auth/register", this.state.credentials)
       .then(res => {
         console.log('bk: Login.js: login: res: ', res)
         localStorage.setItem('token', res.config.data)
--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ const axiosWithAuth = () => {
   const token = localStorage.getItem('token');
 
   return axios.create({
-    baseURL: "http://localhost:7666/api",
+    baseURL: "https://backrecipes.herokuapp.com/api",
     headers: {
       Authorization: token
     }
