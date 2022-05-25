8049330ffa3597083bfac9d59039a44dfe2d1b6f
extrude575757 <nicholas.a.lacapria@gmail.com>
2022-05-25 15:23:47 -0700

    Clicks but does not set props or change the render

--- src/components/pages/title/TitleRender.js
+++ src/components/pages/title/TitleRender.js
@@ -5,18 +5,18 @@ import {TitlePass2} from "../../common"
 import { TitlePass1 } from "../../common";
 import { AppComp } from "../../common";
 import { toggleEditing, setTitle } from "../../../actions/titleActions";
-
+import { Title } from "./Title";
 
 const TitleRender = ({...props}) =>{
     const {editing,title2,setTitle,handleChanges,toggleEditing} = props;
   const title11 = (e) =>{
     return(
-      props.editing && <div>{editing}
+      props.editing && <div>
       <label for='category'>
             Category
           </label>
-        <TitlePass1 handleChanges={handleChanges}title2={title2} setTitle={setTitle} editing={props.editing} />
-  
+       { <TitlePass1 handleChanges={handleChanges}title2={title2} setTitle={setTitle} editing={props.editing} />
+  }
         </div>
     )
   }
@@ -42,17 +42,19 @@ const TitleRender = ({...props}) =>{
              <i
                className="far fa-edit"
               onClick={()=>{
-                 toggleEditing
+                 toggleEditing()
                }}
              />
            </h1>
 
-    ) : (
+    ) : editing && (
       <TitlePass2 title2={props.title2} handleChanges={props.handleChanges} setTitle={props.setTitle} />
     
 
 
-  )}
+  )
+
+  }
   </>
 )}
 const mapStateToProps = ({ titleState }) => {
